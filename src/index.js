21a75556ba9938c2132c43bbe2c41fdda7d8c90b
minfanzhang <zhang.minfan@mail.utoronto.ca>
2021-11-01 19:07:14 -0400

    None

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
